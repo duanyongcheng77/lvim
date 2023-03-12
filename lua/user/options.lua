3edb6934fa9118c715bb42f111991ad9b398bc86
duanyongcheng77 <duanyongcheng77@gmail.com>
2023-03-12 16:50:15 +0800

    重新打开缓冲区恢复光标位置

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,6 +1,15 @@
 local M = {}
 
 M.config = function()
+  vim.api.nvim_create_autocmd("BufReadPost", {
+      pattern = "*",
+      callback = function()
+        if vim.fn.line("'\"") > 0 and vim.fn.line("'\"") <= vim.fn.line("$") then
+          vim.fn.setpos(".", vim.fn.getpos("'\""))
+          vim.cmd("silent! foldopen")
+        end
+      end,
+  })
   -- Theme
   -- =========================================
   require("user.theme").tokyonight()
